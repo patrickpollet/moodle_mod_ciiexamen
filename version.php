9b283ecd583e32aa5d04981511bc384b86253c7f
patrickpollet <pp@patrickpollet.net>
2014-10-17 17:27:45 +0200

    moodle 2.7 add_to_log deprecié . Resolu dans view.php et index.php

--- version.php
+++ version.php
@@ -11,8 +11,8 @@
 
 //$module->version  = 2009071813;  // The current module version (Date: YYYYMMDDXX)
 //bumped for new capabilities (addinstance...) required in Moodle 2.3
-$module->version  = 2014101700;
-$module->requires  = 2011112900;        // Requires this Moodle version (Moodle 2.0 ) 
+$module->version  = 2014101701;
+$module->requires  = 2014051200; // Moodle 2.7.0 is required. (no more add_to_log)
 $module->component = 'mod_ciiexamen';       // Full name of the plugin (used for diagnostics)
 $module->cron     = 60;           // Period for cron to check this module (secs)
 $module->maturity = MATURITY_STABLE; // required for registering to Moodle's database of plugins 
